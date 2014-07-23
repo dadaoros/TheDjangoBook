f46a27cd1737c4a65ed061e97a17306b51a33dee
David Ortegon <dadaoros@hotmail.com>
2014-07-22 19:14:45 -0500

    --thrid commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from mysite.views import hello
+from mysite.views import hello, current_datetime
 
 
 urlpatterns = patterns('',
     url(r'^hello/', hello),
+    url(r'^time/$', current_datetime),
 )
 
 
